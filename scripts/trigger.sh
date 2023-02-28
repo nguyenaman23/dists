e3d830f1128eddada2712d880ebe79166d6588b1
Dichvucoder <119738964+Dichvucoder@users.noreply.github.com>
2023-02-28 12:56:12 +0700

    Update trigger.sh

--- scripts/trigger.sh
+++ scripts/trigger.sh
@@ -8,14 +8,16 @@ asset_json=$(mktemp /tmp/json.XXXXXXX)
 gh api -H "Accept: application/vnd.github.v3+json"  https://api.github.com/repos/$owner/$repo/releases > $asset_json
 
 
-#if jq -r '.[] | select(.tag_name=="0.1") | .assets[].name' $asset_json | grep -G ".tar$";then
+if jq -r '.[] | select(.tag_name=="0.1") | .assets[].name' $asset_json | grep -G ".tar$";then
     echo "Jai ho. we got some unprocessed debfile."
     if $base_dir/dist_handler.sh;then
         $base_dir/remote_handler.sh
     else
         exit 1
     fi
-
-#fi 
+else
+    echo "Jai ho. We got sign"
+    $base_dir/dist_handler.sh
+fi 
 
 
